124cf421383d5db137ac265a3ce9e324adb34912
LilianaC <liliana.castanon@gmail.com>
2024-09-15 17:38:54 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -20,7 +20,7 @@ def reset():
 st.button('🔄 Resetear',on_click=reset)
 
 st.write("¿Cuál es el nombre del elemento químico con el símbolo", df.iloc[num]['Symbol'], "?")
-elemento = st.radio("Selecciona el elemento",lista)
+elemento = st.radio("Selecciona el elemento",lista,disabled=True)
 
 if elemento ==  df.iloc[num]['Elemento']:
     st.write("¡Excelente!")
