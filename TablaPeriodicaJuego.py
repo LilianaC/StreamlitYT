20f36f64cc2feb00aeb8c7f007de2a0b73d4f2f0
LilianaC <liliana.castanon@gmail.com>
2024-09-15 01:22:38 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -8,21 +8,19 @@ import random
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTyeAUixFkE9fiDDCx_Zifmngrjf1_9jjr1Tb7n1twPWiw0tfqd0atb1juO9ncpD5wDrjbBgcHqmfOy/pub?gid=435584327&single=true&output=csv'
 df = pd.read_csv(url)
 
-
-num = random.randint(0, len(df))
-letra = df.iloc[num]['Elemento'][0]
-resultado = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
-lista = resultado.values.tolist()
-nums = df['AtomicNumber'].loc[df['Elemento'].str.startswith(letra)]
-nums = nums.values.tolist()
-diccio = dict(map(lambda i,j : (i,j) , nums,lista))
-
-st.write("¿Cuál es el nombre del elemento químico con el símbolo", df.iloc[num]['Symbol'], "?")
+def juego():
+    num = random.randint(0, len(df))
+    letra = df.iloc[num]['Elemento'][0]
+    resultado = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
+    lista = resultado.values.tolist()
+    st.write("¿Cuál es el nombre del elemento químico con el símbolo", df.iloc[num]['Symbol'], "?")
 
 elemento = st.radio("Selecciona el elemento",lista)
 
 if elemento ==  df.iloc[num]['Elemento']:
     st.balloons()
+else:
+    juego()
 
 
 
