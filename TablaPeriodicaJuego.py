51d22919a12d043eb327a3b19092a82285e4e931
LilianaC <liliana.castanon@gmail.com>
2024-09-15 23:29:54 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -7,16 +7,13 @@ import random
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTyeAUixFkE9fiDDCx_Zifmngrjf1_9jjr1Tb7n1twPWiw0tfqd0atb1juO9ncpD5wDrjbBgcHqmfOy/pub?gid=435584327&single=true&output=csv'
 df = pd.read_csv(url)
-num = random.randint(0, len(df))
 
-
-letra = df.iloc[num]['Elemento'][0]
-
-resultado = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
-
-lista = resultado.values.tolist()
-lista [0:0] = ['🤔']
-
+def juego():
+    num = random.randint(0, len(df))
+    letra = df.iloc[num]['Elemento'][0]
+    resultado = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
+    lista = resultado.values.tolist()
+    return [num,lista]
 
 def reset():
     st.session_state.puntos = 0
@@ -32,11 +29,13 @@ def revision():
         st.session_state.puntos -= 1
         st.write("Puntos",st.session_state.puntos)
     
-st.button('🔄 Resetear',on_click=reset)
 
+num,lista = juego()
+lista [0:0] = ['🤔']
 st.write("¿Cuál es el nombre del elemento químico con el símbolo", df.iloc[num]['Symbol'], "?")
 elemento = st.radio("Selecciona el elemento",lista,on_change=revision)
 st.session_state.puntos = 0
+st.button('🔄 Resetear',on_click=reset)
 
 
 
