f7a4ca68fbcf7096242e726b474184fc07b0a629
LilianaC <liliana.castanon@gmail.com>
2024-09-15 23:51:42 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -7,11 +7,11 @@ import random
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTyeAUixFkE9fiDDCx_Zifmngrjf1_9jjr1Tb7n1twPWiw0tfqd0atb1juO9ncpD5wDrjbBgcHqmfOy/pub?gid=435584327&single=true&output=csv'
 df = pd.read_csv(url)
-st.session_state.num=0
+
 
 def juego():
     
-    st.session_state.num = random.randint(0, len(df))
+    num = random.randint(0, len(df))
     letra = df.iloc[num]['Elemento'][0]
     resultado = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
     lista = resultado.values.tolist()
