58e6adc5a806ce4642656c2b6ba11322cd1b3f6c
LilianaC <liliana.castanon@gmail.com>
2024-09-19 15:06:18 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -7,7 +7,7 @@ import random
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTyeAUixFkE9fiDDCx_Zifmngrjf1_9jjr1Tb7n1twPWiw0tfqd0atb1juO9ncpD5wDrjbBgcHqmfOy/pub?gid=435584327&single=true&output=csv'
 df = pd.read_csv(url)
 
-lista=[]
+#lista=[]
 
 juego = st.button('🔄 Juego nuevo')
 
