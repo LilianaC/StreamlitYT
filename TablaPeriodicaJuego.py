9aed4a91f7a9bbf7c343d941a4cec7573d96c62a
LilianaC <liliana.castanon@gmail.com>
2024-09-19 13:00:56 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -7,30 +7,42 @@ import random
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTyeAUixFkE9fiDDCx_Zifmngrjf1_9jjr1Tb7n1twPWiw0tfqd0atb1juO9ncpD5wDrjbBgcHqmfOy/pub?gid=435584327&single=true&output=csv'
 df = pd.read_csv(url)
 
+juego = st.button('🔄 Juego nuevo')
+
 if "num" not in st.session_state:
     st.session_state.num = random.randint(0, len(df))
 
 if "puntos" not in st.session_state:
     st.session_state.puntos = 0
 
-elemento = df.iloc[st.session_state.num]['Elemento']
-symbol = df.iloc[st.session_state.num]['Symbol']
-letra = df.iloc[st.session_state.num]['Elemento'][0]
-pistas = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
-lista = pistas.values.tolist()
-lista.insert(0, "🤔")
+if "juego_state" not in st.session_state:
+    st.session_state.juego_state = False
 
-st.write("¿Cuál es el nombre del elemento químico con el símbolo ",symbol, "?")
-respuesta = st.radio("Selecciona el elemento",lista)
+if juego or st.session_state.juego_state:
+    elemento = df.iloc[st.session_state.num]['Elemento']
+    symbol = df.iloc[st.session_state.num]['Symbol']
+    letra = df.iloc[st.session_state.num]['Elemento'][0]
+    pistas = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
+    lista = pistas.values.tolist()
+    lista.insert(0, "🤔")
+    st.write("¿Cuál es el nombre del elemento químico con el símbolo ",symbol, "?")
+    respuesta = st.radio("Selecciona el elemento",lista)
+    
+    if respuesta ==  elemento:
+        st.write("¡Excelente!")
+        st.session_state.puntos += 1
+        st.write("Puntos",st.session_state.puntos)
+    else:
+        st.write("Respuesta incorrecta")
+        st.session_state.puntos -= 1
+        st.write("Puntos",st.session_state.puntos)
 
-if respuesta ==  elemento:
-    st.write("¡Excelente!")
-    st.session_state.puntos += 1
-    st.write("Puntos",st.session_state.puntos)
-else:
-    st.write("Respuesta incorrecta")
-    st.session_state.puntos -= 1
-    st.write("Puntos",st.session_state.puntos)
+    
+
+
+
+
+
     
     
 
