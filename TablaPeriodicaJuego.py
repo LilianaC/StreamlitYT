6e68340785016d4b6cc474d95cec660d6c57a543
LilianaC <liliana.castanon@gmail.com>
2024-09-19 15:02:54 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -7,7 +7,7 @@ import random
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTyeAUixFkE9fiDDCx_Zifmngrjf1_9jjr1Tb7n1twPWiw0tfqd0atb1juO9ncpD5wDrjbBgcHqmfOy/pub?gid=435584327&single=true&output=csv'
 df = pd.read_csv(url)
 
-lista=[]
+#lista=[]
 
 juego = st.button('🔄 Juego nuevo')
 
@@ -29,7 +29,7 @@ if juego or st.session_state.juego_state:
     letra = df.iloc[num]['Elemento'][0]
     pistas = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
     
-    lista.insert(0, "🤔")
+    #lista.insert(0, "🤔")
     lista = pistas.values.tolist()
     
     st.write("¿Cuál es el nombre del elemento químico con el símbolo ",symbol, "?")
