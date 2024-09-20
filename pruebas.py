5101e2248b6cc0a6b17edbfc5c0c958b2d6ce9ab
LilianaC <liliana.castanon@gmail.com>
2024-09-20 08:11:53 -0600

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import random
+import time
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTyeAUixFkE9fiDDCx_Zifmngrjf1_9jjr1Tb7n1twPWiw0tfqd0atb1juO9ncpD5wDrjbBgcHqmfOy/pub?gid=435584327&single=true&output=csv'
 df = pd.read_csv(url)
@@ -17,12 +18,14 @@ lista.insert(0,"😁")
     
 st.write("¿Cuál es el nombre del elemento químico con el símbolo ",symbol, "?")
 respuesta = st.radio("Selecciona el elemento",lista)#,index=None
-st.stop()
+#st.stop()
 
 if respuesta ==  elemento:
   st.write("¡Excelente!")
+  time.sleep(2)
   st.rerun()
 else:
   st.write("Respuesta incorrecta")
+  time.sleep(2)
   st.rerun()
   
