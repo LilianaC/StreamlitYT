7c0c635f41dc97f877d7e5f0272f67eef3bc9e8e
LilianaC <liliana.castanon@gmail.com>
2024-09-21 16:06:27 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -28,25 +28,17 @@ letra = st.session_state.elemento[0]
 resultado = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
 lista = resultado.values.tolist()
 # Ask the user to select the symbol for the randomly selected element
+correct_element = st.session_state.elemento
+
 selected_element = st.selectbox(
     f'🤔 ¿Cuál es el elemento para {st.session_state.simbolo}?',
     [''] + lista) # Here we are using the 'Symbol' column of the dataframe as the options for the selectbox
 
-if selected_element:
-    # Get the correct symbol for the selected element
-    correct_element = st.session_state.elemento
-    st.write(correct_element)
-
-    # Check if the selected symbol is correct
-    if selected_element == correct_element:
-        st.write('Correct! The symbol for', st.session_state.elemento, 'is', correct_element)
-        #st.session_state.num = random.randint(0, 118)
-        #st.session_state.elemento = df.iloc[st.session_state.num]['Elemento']
-        st.session_state.correct = True
-        #st.rerun() # Rerun the script to ask for another element
-    else:
-        st.write('Incorrect. The symbol for', st.session_state.elemento, 'is', correct_element)
-        st.session_state.correct = False
+if selected_element == correct_element:
+    st.success("Correct!")
+else:
+    st.error("Incorrect!")
+        
         
 #if st.button("🫡 Comenzar de nuevo"):
 #    st.session_state.correct = True
