9403e751200e7b363e1a73b5b481cc724a542fa8
LilianaC <liliana.castanon@gmail.com>
2024-09-21 12:51:33 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -13,29 +13,37 @@ if 'correct' not in st.session_state:
 if 'num' not in st.session_state:
     st.session_state.num = random.randint(0, 118)
 
+if 'elemento' not in st.session_state:
+    st.session_state.elemento = df.iloc[st.session_state.num]['Elemento']
+
+if 'simbolo' not in st.session_state:
+    st.session_state.simbolo = df.iloc[st.session_state.num]['Symbol']
+
+
+
 # If the previous answer was correct, randomly select a new element
 if st.session_state.correct:
     st.session_state.num = random.randint(0, 118)
     #st.session_state.Symbol = df.iloc[st.session_state.num]['Symbol']
 
-letra = df.iloc[st.session_state.num]['Elemento'][0]
+letra = st.session_state.elemento[0]
 resultado = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
 lista = resultado.values.tolist()
 # Ask the user to select the symbol for the randomly selected element
 selected_element = st.selectbox(
-    f'¿Cuál es el elemento para {df.iloc[st.session_state.num]['Symbol']}?',
+    f'🤔 ¿Cuál es el elemento para {st.session_state.simbolo}?',
     [''] + lista) # Here we are using the 'Symbol' column of the dataframe as the options for the selectbox
 
 if selected_element:
     # Get the correct symbol for the selected element
-    correct_element = df.iloc[st.session_state.num]['Elemento']
+    correct_element = st.session_state.elemento
     st.write(correct_element)
 
     # Check if the selected symbol is correct
     if selected_element == correct_element:
-        st.write('Correct! The symbol for', df.iloc[st.session_state.num]['Elemento'], 'is', correct_element)
+        st.write('Correct! The symbol for', st.session_state.elemento, 'is', correct_element)
         st.session_state.correct = True
         st.rerun() # Rerun the script to ask for another element
     else:
-        st.write('Incorrect. The symbol for', f.iloc[st.session_state.num]['Elemento'], 'is', correct_element)
+        st.write('Incorrect. The symbol for', st.session_state.elemento, 'is', correct_element)
         st.session_state.correct = False
