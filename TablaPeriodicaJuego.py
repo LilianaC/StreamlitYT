cd7ac60ef009ea675acbc63622d5f5d8c789d0a9
LilianaC <liliana.castanon@gmail.com>
2024-09-21 16:18:08 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -40,8 +40,8 @@ if selected_element:
 
 if st.session_state.correct:
     st.session_state.num = random.randint(0, 118)
-    st.session_state.simbolo = df.iloc[st.session_state.num]['Symbol']
-    st.session_state.elemento = df.iloc[st.session_state.num]['Elemento']
+    #st.session_state.simbolo = df.iloc[st.session_state.num]['Symbol']
+    #st.session_state.elemento = df.iloc[st.session_state.num]['Elemento']
 
 
 #if st.session_state.correct:
