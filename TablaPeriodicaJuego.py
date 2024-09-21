722c4f13d3fc62943dd3cb28823822ce98e5ce27
LilianaC <liliana.castanon@gmail.com>
2024-09-21 13:04:12 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -6,10 +6,6 @@ import random
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTyeAUixFkE9fiDDCx_Zifmngrjf1_9jjr1Tb7n1twPWiw0tfqd0atb1juO9ncpD5wDrjbBgcHqmfOy/pub?gid=435584327&single=true&output=csv'
 df = pd.read_csv(url)
 
-# Initialize session state
-if 'correct' not in st.session_state:
-    st.session_state.correct = True
-
 if 'num' not in st.session_state:
     st.session_state.num = random.randint(0, 118)
 
@@ -18,8 +14,6 @@ if 'elemento' not in st.session_state:
 
 if 'simbolo' not in st.session_state:
     st.session_state.simbolo = df.iloc[st.session_state.num]['Symbol']
-
-
 
 # If the previous answer was correct, randomly select a new element
 if st.session_state.correct:
@@ -42,13 +36,11 @@ if selected_element:
     # Check if the selected symbol is correct
     if selected_element == correct_element:
         st.write('Correct! The symbol for', st.session_state.elemento, 'is', correct_element)
-        st.session_state.correct = True
-        st.session_state.num = random.randint(0, 118)
-        #st.rerun() # Rerun the script to ask for another element
+        #st.session_state.num = random.randint(0, 118)
+        st.rerun() # Rerun the script to ask for another element
     else:
         st.write('Incorrect. The symbol for', st.session_state.elemento, 'is', correct_element)
-        st.session_state.correct = False
-
+        
 if st.button("🫡 Comenzar de nuevo"):
     st.session_state.correct = True
     #st.rerun()
