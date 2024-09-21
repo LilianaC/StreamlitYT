cbebc0b644243e54f00fb531ee2742ea6361322a
LilianaC <liliana.castanon@gmail.com>
2024-09-20 23:58:07 -0600

    Update AISuggestions.py

--- AISuggestions.py
+++ AISuggestions.py
@@ -8,25 +8,30 @@ df = pd.DataFrame({
     'Symbol': ['H', 'He', 'Li', 'Be', 'B']
 })
 
-correct = True
+# Initialize session state
+if 'correct' not in st.session_state:
+    st.session_state.correct = True
+if 'element' not in st.session_state:
+    st.session_state.element = np.random.choice(df['Element'])
 
-while correct:
-    # Randomly select an element
-    element = np.random.choice(df['Element'])
+# If the previous answer was correct, randomly select a new element
+if st.session_state.correct:
+    st.session_state.element = np.random.choice(df['Element'])
 
-    # Ask the user to select the symbol for the randomly selected element
-    selected_symbol = st.selectbox(
-        f'What is the symbol for {element}?',
-        [''] + list(df['Symbol'])) # Here we are using the 'Symbol' column of the dataframe as the options for the selectbox
+# Ask the user to select the symbol for the randomly selected element
+selected_symbol = st.selectbox(
+    f'What is the symbol for {st.session_state.element}?',
+    [''] + list(df['Symbol'])) # Here we are using the 'Symbol' column of the dataframe as the options for the selectbox
 
-    # If a symbol is selected, check if it is correct and display a message
-    if selected_symbol:
-        # Get the correct symbol for the selected element
-        correct_symbol = df[df['Element'] == element]['Symbol'].values[0]
+# If a symbol is selected, check if it is correct and display a message
+if selected_symbol:
+    # Get the correct symbol for the selected element
+    correct_symbol = df[df['Element'] == st.session_state.element]['Symbol'].values[0]
 
-        # Check if the selected symbol is correct
-        if selected_symbol == correct_symbol:
-            st.write('Correct! The symbol for', element, 'is', correct_symbol)
-        else:
-            st.write('Incorrect. The symbol for', element, 'is', correct_symbol)
-            correct = False
+    # Check if the selected symbol is correct
+    if selected_symbol == correct_symbol:
+        st.write('Correct! The symbol for', st.session_state.element, 'is', correct_symbol)
+        st.session_state.correct = True
+    else:
+        st.write('Incorrect. The symbol for', st.session_state.element, 'is', correct_symbol)
+        st.session_state.correct = False
