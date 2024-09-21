5191b2acf8c781bbd7093ac78bdb4f564206c148
LilianaC <liliana.castanon@gmail.com>
2024-09-20 23:55:03 -0600

    Update AISuggestions.py

--- AISuggestions.py
+++ AISuggestions.py
@@ -8,21 +8,25 @@ df = pd.DataFrame({
     'Symbol': ['H', 'He', 'Li', 'Be', 'B']
 })
 
-# Randomly select an element
-element = np.random.choice(df['Element'])
+correct = True
 
-# Ask the user to select the symbol for the randomly selected element
-selected_symbol = st.selectbox(
-    f'What is the symbol for {element}?',
-    [''] + list(df['Symbol'])) # Here we are using the 'Symbol' column of the dataframe as the options for the selectbox
+while correct:
+    # Randomly select an element
+    element = np.random.choice(df['Element'])
 
-# If a symbol is selected, check if it is correct and display a message
-if selected_symbol:
-    # Get the correct symbol for the selected element
-    correct_symbol = df[df['Element'] == element]['Symbol'].values[0]
+    # Ask the user to select the symbol for the randomly selected element
+    selected_symbol = st.selectbox(
+        f'What is the symbol for {element}?',
+        [''] + list(df['Symbol'])) # Here we are using the 'Symbol' column of the dataframe as the options for the selectbox
 
-    # Check if the selected symbol is correct
-    if selected_symbol == correct_symbol:
-        st.write('Correct! The symbol for', element, 'is', correct_symbol)
-    else:
-        st.write('Incorrect. The symbol for', element, 'is', correct_symbol)
+    # If a symbol is selected, check if it is correct and display a message
+    if selected_symbol:
+        # Get the correct symbol for the selected element
+        correct_symbol = df[df['Element'] == element]['Symbol'].values[0]
+
+        # Check if the selected symbol is correct
+        if selected_symbol == correct_symbol:
+            st.write('Correct! The symbol for', element, 'is', correct_symbol)
+        else:
+            st.write('Incorrect. The symbol for', element, 'is', correct_symbol)
+            correct = False
