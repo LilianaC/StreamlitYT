8b99dba2734a309202281d8c9bbc9c5d84eb9b2e
LilianaC <liliana.castanon@gmail.com>
2024-09-21 17:25:19 -0600

    Update AISuggestions.py

--- AISuggestions.py
+++ AISuggestions.py
@@ -1,7 +1,18 @@
 import streamlit as st
 
+# Define your custom CSS
+custom_css = """
+<style>
+[data-baseweb="select"] > div {
+    font-size: 20px;
+    color: red;
+    background-color: yellow;
+}
+</style>
+"""
+
+# Inject custom CSS with st.markdown()
+st.markdown(custom_css, unsafe_allow_html=True)
+
 # Render the selectbox
 option = st.selectbox('Select an option', ['Option 1', 'Option 2', 'Option 3'])
-
-# Call st.rerun()
-st.rerun()
