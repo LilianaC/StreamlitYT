e59db92a11f545ff2d1ba5374717a8e9a5ca4813
LilianaC <liliana.castanon@gmail.com>
2024-09-21 13:05:17 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -36,8 +36,9 @@ if selected_element:
     # Check if the selected symbol is correct
     if selected_element == correct_element:
         st.write('Correct! The symbol for', st.session_state.elemento, 'is', correct_element)
-        #st.session_state.num = random.randint(0, 118)
-        st.rerun() # Rerun the script to ask for another element
+        st.session_state.num = random.randint(0, 118)
+        st.session_state.elemento = df.iloc[st.session_state.num]['Elemento']
+        #st.rerun() # Rerun the script to ask for another element
     else:
         st.write('Incorrect. The symbol for', st.session_state.elemento, 'is', correct_element)
         
