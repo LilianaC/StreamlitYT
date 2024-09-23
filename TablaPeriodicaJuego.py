10198e1303c4587f36a768a8894d6340a8f0c36f
LilianaC <liliana.castanon@gmail.com>
2024-09-23 12:52:12 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -31,7 +31,7 @@ resultado = df['Elemento'].loc[df['Elemento'].str.startswith(letra)]
 lista = resultado.values.tolist()
 correct_element = st.session_state.elemento
 
-st.title(":blue[Símbolos de los elementos químicos 🧪]"])
+st.title(":blue[Símbolos de los elementos químicos 🧪]")
 
 
 selected_element = st.selectbox(
