447cfb7d43c0e422393fb8715d7a81a9f8cca65b
LilianaC <liliana.castanon@gmail.com>
2024-09-23 16:01:01 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -5,12 +5,15 @@ import random
 import time
 
 # Custom CSS to inject larger font size for selectbox
-st.markdown("""
+tabs_font_css = """
 <style>
-    .stselectbox > div > div { font-size: 25px; }
+div[class*="stTextArea"] label {
+  font-size: 26px;
+  color: red;
+}
 </style>
-""", unsafe_allow_html=True)
-
+"""
+st.write(tabs_font_css, unsafe_allow_html=True)
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTyeAUixFkE9fiDDCx_Zifmngrjf1_9jjr1Tb7n1twPWiw0tfqd0atb1juO9ncpD5wDrjbBgcHqmfOy/pub?gid=435584327&single=true&output=csv'
 df = pd.read_csv(url)
