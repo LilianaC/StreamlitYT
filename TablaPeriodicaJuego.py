c6b230304346c3709eba9b7f542a97d916d51d39
LilianaC <liliana.castanon@gmail.com>
2024-09-23 16:02:56 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -7,7 +7,7 @@ import time
 # Custom CSS to inject larger font size for selectbox
 tabs_font_css = """
 <style>
-div[class*="stTextArea"] label {
+div[class*="stSelectbox"] label {
   font-size: 26px;
   color: red;
 }
