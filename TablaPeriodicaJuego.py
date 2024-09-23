cb2a4b3f7cb849170650aad6528750b30b1a88da
LilianaC <liliana.castanon@gmail.com>
2024-09-23 17:23:34 -0600

    Update TablaPeriodicaJuego.py

--- TablaPeriodicaJuego.py
+++ TablaPeriodicaJuego.py
@@ -46,7 +46,7 @@ selected_element = st.selectbox(
     f'🤔 ¿Cuál es el elemento para {st.session_state.simbolo}?',
     [''] + lista) # Here we are using the 'Symbol' column of the dataframe as the options for the selectbox
 
-st.subheader(":violet[Algunas características de este elemento son:]")
+st.subheader(":violet[Algunas características de este elemento son:]",,divider='orange')
 st.subheader(f"El :green[número atómico] es: {int(df.iloc[st.session_state.num]['AtomicNumber'])}")
 st.subheader(df.iloc[st.session_state.num]['Fase'], df.iloc[st.session_state.num]['Clasifica'])
 st.subheader(df.iloc[st.session_state.num]['Apariencia'])
